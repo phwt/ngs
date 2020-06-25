3779aa7d3348e8b435fb0ed431cb3ce4100b2f38
phwt <phwt.smwt@gmail.com>
2020-06-25 22:51:28 +0700

    Challenge 1/2

--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -1,9 +1,9 @@
-const theOneFunc = () => {};
-
-setTimeout(theOneFunc, 4 * 1000);
+const theOneFunc = (sec) => {
+    console.log(`Hello after ${sec} seconds`)
+};
 
 // Hello after 4 seconds
-
+setTimeout(theOneFunc, 4 * 1000, "4");
 // Hello after 8 seconds
-
+setTimeout(theOneFunc, 8 * 1000, "8");
 // You can define only ONE function
--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -1,3 +1,9 @@
+let counter = 0;
+let interval = setInterval(() => {
+    console.log("Hello World!")
+    if (++counter == 5) clearInterval(interval)
+}, 1000);
+
 // Print "Hello World"
 // Every second
 // And stop after 5 times
