c3db5d4cff05aab3819d0fbc56f4f51c1b905c1d
phwt <phwt.smwt@gmail.com>
2020-06-28 11:42:07 +0700

    Windows path

--- 4-modules/5-errors/1-loop.js
+++ 4-modules/5-errors/1-loop.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const fs = require('fs');
 
-const files = ['.bash_profile', '.npmrc'];
+const files = ['file.txt'];
 
 files.forEach(file => {
-  const filePath = path.resolve(process.env.HOME, file);
+  const filePath = path.resolve(process.env.USERPROFILE, file);
   const data = fs.readFileSync(filePath);
   console.log('File data is', data);
 });
