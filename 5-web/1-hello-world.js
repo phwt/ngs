6f2f5928bcc4a811333586cea51e1564d1695224
phwt <phwt.smwt@gmail.com>
2020-06-28 12:06:17 +0700

    Update 1-hello-world.js

--- 5-web/1-hello-world.js
+++ 5-web/1-hello-world.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
-});
+const requestListener = (req, res) => {
+  res.write('Hello World\n');
+  res.end();
+  // Equivalent to
+  // res.end('Hello World\n');
+}
+
+// const server = http.createServer(requestListener);
+const server = http.createServer();
+server.on('request', requestListener)
 
 server.listen(4242, () => {
   console.log('Server is running...');
